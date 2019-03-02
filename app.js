6ca56230a1fc9a5e4f68782463b8a19dbf3b2421
Latrop <32779759+Latrop1a@users.noreply.github.com>
2019-03-02 13:46:59 +0100

    dum

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 let budgetController = (function() {
 
-  let x = 23
+  let x = 23;
   
   let add = function(a) {
     return x + a;
-  }
+  };
 
   return {
     publicAdd: function(b) {
       console.log(add(b));
     }
-  }
-})()
+  };
+})();
 
 
 
 let UIController = (function() {
 
  
-})()
+})();
 
 
 
 let controller = (function(budgetCtrl, UICtrl) {
 
-  budgetCtrl.publicAdd(5)
+  budgetCtrl.publicAdd(5);
 
-})(budgetController, UIController)
-
+})(budgetController, UIController);
 
 
