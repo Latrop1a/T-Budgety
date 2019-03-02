4928c7b76712c54981f02e9e4a1f978d60d7e07e
Latrop <32779759+Latrop1a@users.noreply.github.com>
2019-03-02 13:39:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+let budgetController = (function() {
+
+  let x = 23
+  
+  let add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicAdd: function(b) {
+      console.log(add(b));
+    }
+  }
+})()
+
+
+
+let UIController = (function() {
+
+ 
+})()
+
+
+
+let controller = (function(budgetCtrl, UICtrl) {
+
+  budgetCtrl.publicAdd(5)
+
+})(budgetController, UIController)
+
+
+
